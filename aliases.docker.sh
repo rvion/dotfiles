bfa769a1389a9c85367746558e775a8b02789a49
rvion <vion.remi@gmail.com>
2015-05-03 18:45:16 -0400

    [docker] aliases + socker-compose init script

--- aliases.docker.sh
+++ aliases.docker.sh
@@ -1,2 +1,4 @@
 alias drun="docker run --rm -it"
 alias inubuntu="docker run --rm -it ubuntu"
+alias dc="docker-compose"
+alias dm="docker-machine"
---
+++ docker-compose.sh
@@ -0,0 +1,3 @@
+curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > docker-compose
+chmod +x docker-compose
+mv docker-compose /usr/local/bin/docker-compose
