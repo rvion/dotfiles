22a3248c7839eb5d8503dc3f7399f08a85210949
rvion <vion.remi@gmail.com>
2015-05-03 15:34:55 -0400

    cleaner and nicer install

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
- 
+set -e
+
+. ~/.dotfiles/function.safely_symlink_file.sh # --source-only 
+
+setup_symlinks () {
+    local overwrite_all=false backup_all=false skip_all=false
+    safely_symlink_file ~/.dotfiles/.gitconfig ~/.gitconfig
+    safely_symlink_file ~/.dotfiles/.zshrc     ~/.zshrc
+}
+
+setup_symlinks
