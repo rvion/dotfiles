43944843c35cd72e91a4371803aebdb52007b2d3
rvion <vion.remi@gmail.com>
2015-05-03 17:03:22 -0400

    add curl, wget, docker to setup_ubuntu

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -2,5 +2,5 @@ set -e # Abort after first error
 set -x # Log each command
 
 apt-get update -qq
-apt-get install vim tmux git zsh
+apt-get install vim tmux git zsh docker.io curl wget -yq
 chsh -s $(which zsh)
