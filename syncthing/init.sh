7f26813b599f42ef815afb7fa0f42fd5b0fa6ed1
rvion <vion.remi@gmail.com>
2015-05-04 07:10:23 -0400

    [sync] add syncthing-inotify

--- syncthing/init.sh
+++ syncthing/init.sh
@@ -1,1 +1,2 @@
 curl -L https://github.com/syncthing/syncthing/releases/download/v0.11.2/syncthing-linux-amd64-v0.11.2.tar.gz | tar xvz 
+curl -L https://github.com/syncthing/syncthing-inotify/releases/download/v0.6.1/syncthing-inotify-linux-amd64-v0.6.1.tar.gz | tar xvz 
