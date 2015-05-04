7d493cb26bdd61edd54d72be7858f92e522939d7
rvion <vion.remi@gmail.com>
2015-05-04 05:30:29 -0400

    [tools] install unzip when on ubuntu

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -2,5 +2,5 @@ set -e # Abort after first error
 set -x # Log each command
 
 apt-get update -qq
-apt-get install vim tmux git zsh docker.io curl wget htop -yq
+apt-get install vim tmux git zsh docker.io curl wget htop unzip -yq
 chsh -s $(which zsh)
