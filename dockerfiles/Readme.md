1d9843dc4645a9158df77ec344f026a0cd8c65fc
rvion <vion.remi@gmail.com>
2015-05-11 12:07:33 -0400

    [docker] xforwarding container working

---
+++ dockerfiles/Readme.md
@@ -0,0 +1,5 @@
+# Dockerfiles
+
+This folder contains practical Dockerfiles to ease
+developping graphical application directly
+on remote machines
--- dockerfiles/testXforwarding/run.sh
+++ dockerfiles/testXforwarding/run.sh
@@ -1,4 +1,6 @@
 docker run -ti --rm \
-       -e DISPLAY=$DISPLAY \
+       -e DISPLAY \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
+       -v $HOME/.Xauthority:/root/.Xauthority \
+       --net=host \
        rvion/xforward
